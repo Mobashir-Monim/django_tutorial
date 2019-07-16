09d609bd85a9f7859df3aee6e4e12fa2f29d546f
Mobashir_Monim@bitbucket.org <mobashirmonim@gmail.com>
2019-07-16 14:29:08 +0600

    Implement sudoku template

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 1',
+        'content': 'This is the content Blog Post 1',
+        'date_posted': 'July 15, 2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'This is the content for Blog Post 2',
+        'date_posted': 'July 16, 2019'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', { 'title': 'About'})
+
+def sudoku(request):
+    context = {
+        'rows': [
+            [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [2, 3, 4, 5, 6, 7, 8, 9, 1],
+            [3, 4, 5, 6, 7, 8, 9, 1, 2],
+            [4, 5, 6, 7, 8, 9, 1, 2, 3],
+            [5, 6, 7, 8, 9, 1, 2, 3, 4],
+            [6, 7, 8, 9, 1, 2, 3, 4, 5],
+            [7, 8, 9, 1, 2, 3, 4, 5, 6],
+            [8, 9, 1, 2, 3, 4, 5, 6, 7],
+            [9, 1, 2, 3, 4, 5, 6, 7, 8],
+        ]
+    }
+    return render(request, 'blog/sudoku.html', context)
